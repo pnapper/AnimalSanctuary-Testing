d5508a6fa41a6e32e662d9f127e6e1351d59be60
Patrick Napper <patrick@patricknapper.com>
2018-01-11 09:13:24 -0800

    create initial project with models, controllers and empty test and repo files.

---
+++ AnimalSanctuary.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnimalSanctuary.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
